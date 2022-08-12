b644254c4261820ffcd01364bf34ddbb97d0307e
Tiny-Prince <rlaxodnjs6574@gmail.com>
2022-08-12 22:37:12 +0900

    22.08.12 Ground 수정

--- MercuryProject/CGround.cpp
+++ MercuryProject/CGround.cpp
@@ -68,26 +68,7 @@ void CGround::OnCollision(CCollider* _pOther)
 	CObject* pOtherObj = _pOther->GetObj();
 	if (pOtherObj->GetType() == GROUP_TYPE::PLAYER)
 	{
-		CRigidBody* pRigid = pOtherObj->GetRigidBody();
-		Vec2 vObjPos = _pOther->GetFinalPos();
-		Vec2 vObjScale = _pOther->GetScale();
-
-		Vec2 vPos = GetCollider()->GetFinalPos();
-		Vec2 vScale = GetCollider()->GetScale();
-		if (pRigid == nullptr || pRigid->IsGround() == false)
-			return;
-		if (vPos.y > vObjPos.y)
-			return;
-
-		// 통과한 길이
-		// 함수의 연산결과를 바로 수식으로 사용하면 디버깅시 어려움이 있으니, 지역변수로 먼저 받아놓고 사용.
-		float fLen = abs(vObjPos.y - vPos.y);
-		float fValue = ((vObjScale.y / 2.f) + (vScale.y / 2.f)) - fLen;
-
-		vObjPos = pOtherObj->GetPos();
-		vObjPos.y -= fValue;
-
-		pOtherObj->SetPos(vObjPos);
+		
 	}
 }
 void CGround::OnCollisionExit(CCollider* _pOther) 
@@ -95,28 +76,6 @@ void CGround::OnCollisionExit(CCollider* _pOther)
 	CObject* pOtherObj = _pOther->GetObj();
 	if (pOtherObj->GetType() == GROUP_TYPE::PLAYER)
 	{
-		CRigidBody* pRigid = pOtherObj->GetRigidBody();
-
-		Vec2 vObjPos = _pOther->GetFinalPos();
-		Vec2 vObjScale = _pOther->GetScale();
-
-		Vec2 vPos = GetCollider()->GetFinalPos();
-		Vec2 vScale = GetCollider()->GetScale();
-		if (pRigid == nullptr || pRigid->IsGround() == false)
-			return;
-		if (vPos.y > vObjPos.y)
-			return;
-
-
-
-		// 통과한 길이
-		// 함수의 연산결과를 바로 수식으로 사용하면 디버깅시 어려움이 있으니, 지역변수로 먼저 받아놓고 사용.
-		float fLen = abs(vObjPos.y - vPos.y);
-		float fValue = (vObjScale.y / 2.f + vScale.y / 2.f) - fLen;
-
-		vObjPos = pOtherObj->GetPos();
-		vObjPos.y -= fValue;
-
-		pOtherObj->SetPos(vObjPos);
+		
 	}
 }
